df61ba996fc303e70f0dfbed18b5a215e2521dcc
Chinmayee0409 <cg3690@srmist.edu.in>
2026-04-15 19:13:16 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Bogie {
+    String type;
+    int capacity;
+
+    Bogie(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+    }
+}
+
+public class TrainConsistUC10 {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("Sleeper", 72));
+
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total Seating Capacity: " + totalSeats);
+    }
+}
